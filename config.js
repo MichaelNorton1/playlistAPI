41376755eaef584a02d0c013da020173cc9e798e
michael <michael.norton1213@gmail.com>
2021-12-07 19:58:48 -0500

    serverside

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var client_id = "384822665d1f4b9a9805ad736b1bb747";
 const client_secret = "191e8dde87d9455282abc709757bead0";
-const key = "oDferHkYfGM2gmauqpe4I488IkqvZGUlT3ur";
+//const key = "oDferHkYfGM2gmauqpe4I488IkqvZGUlT3ur";
+const key = "vQhvMrBDBanyrfaC2QDcsfM6sFQgPyt25Ewj";
 export default { client_id, client_secret, key };
